b37f3eeb3becda2520d6cc32bcceb5de26e522bc
lsaffin <l.saffin@pgr.reading.ac.uk>
2020-01-30 16:51:16 -0400

    Corrected extracting date string when loading flight data

--- util.py
+++ util.py
@@ -6,8 +6,8 @@ flight_info = pd.read_csv('obs/flight_information.csv')
 
 
 def load_flight(flight_number, frequency=1):
-    datestr = flight_info.loc[flight_info['Flight Number'] == flight_number]['Date'][0].replace('-', '')
-    filename = 'obs/core_masin_{}_r002_flight{}_1hz.nc'.format(datestr, flight_number)
+    datestr = flight_info['Date'][flight_info['Flight Number'] == flight_number].values[0].replace('-', '')
+    filename = 'obs/core_masin_{}_r002_flight{}_{}hz.nc'.format(datestr, flight_number, frequency)
 
     # because of how the flags are stored (units is '1b') decoding cf times
     # fails
