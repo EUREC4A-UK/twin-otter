3a521ad5f0f354ba5fb7e216056929548473667c
lsaffin <l.saffin@leeds.ac.uk>
2020-02-04 15:34:13 -0400

    Put filename generation into a separate function to loading data

--- twinotter/__init__.py
+++ twinotter/__init__.py
@@ -4,39 +4,19 @@ from pathlib import Path
 import re
 
 
-# flight_info = pd.read_csv('obs/flight_information.csv')
+flight_info = pd.read_csv('obs/flight_information.csv')
 
 MASIN_CORE_FORMAT = "core_masin_{date}_r{revision}_flight{flight_num}_{freq}hz.nc"
 MASIN_CORE_RE = "core_masin_(?P<date>\d{8})_r(?P<revision>\d{3})_flight(?P<flight_num>\d{3})_(?P<freq>\d+)hz\.nc"
 
-def load_flight(flight_data_path, frequency=1, revision="most_recent", debug=False):
+
+def load_flight(flight_number, frequency=1, revision="most_recent", debug=False):
+    filename = generate_filename(flight_number, frequency)
+
     if revision == "most_recent":
         revision = "*"
     else:
         revision = "{:03d}".format(revision)
-
-    fn_pattern = MASIN_CORE_FORMAT.format(
-        date="*", revision=revision, flight_num="*", freq=frequency
-    )
-    files = list((Path(flight_data_path)/"MASIN").glob(fn_pattern))
-
-    meta = {}
-    for file in files:
-        meta[file] = re.match(MASIN_CORE_RE, file.name).groupdict()
-
-    if len(files) == 0:
-        raise Exception("Couldn't find MASIN data in `{}/MASIN`, please place"
-                        " data there.".format(flight_data_path))
-
-    if len(files) > 1:
-        if revision == "*":
-            filename = sorted(files, key=lambda v: meta[v]['revision'], reverse=True)[0]
-        else:
-            raise Exception("More than one MASIN file was found: `{}`".format(
-                ", ".join(files)
-            ))
-    else:
-        filename = files[0]
 
     ds = xr.open_dataset(filename, decode_cf=False)
     if debug:
@@ -70,7 +50,7 @@ def flight_leg_index(flight_number, leg_name, leg_number=0):
             select which leg you want. Default is zero
 
     Returns:
-        slice: 
+        slice:
     """
     legs = pd.read_csv('obs/legs_flight{}.csv'.format(flight_number))
     idx = legs[legs['Type'] == leg_name].index[leg_number]
@@ -78,3 +58,41 @@ def flight_leg_index(flight_number, leg_name, leg_number=0):
     end = legs['End'][idx]
 
     return slice(start, end)
+
+
+def generate_filename(flight_number, frequency):
+    # fn_pattern = MASIN_CORE_FORMAT.format(
+    #     date="*", revision=revision, flight_num="*", freq=frequency
+    # )
+    # files = list((Path(flight_data_path)/"MASIN").glob(fn_pattern))
+    #
+    # meta = {}
+    # for file in files:
+    #     meta[file] = re.match(MASIN_CORE_RE, file.name).groupdict()
+    #
+    # if len(files) == 0:
+    #     raise Exception("Couldn't find MASIN data in `{}/MASIN`, please place"
+    #                     " data there.".format(flight_data_path))
+    #
+    # if len(files) > 1:
+    #     if revision == "*":
+    #         filename = sorted(files, key=lambda v: meta[v]['revision'], reverse=True)[0]
+    #     else:
+    #         raise Exception("More than one MASIN file was found: `{}`".format(
+    #             ", ".join(files)
+    #         ))
+    # else:
+    #     filename = files[0]
+
+    # Find the entry matching the flight number
+    # This currently assumes there is at most one entry for each flight number,
+    # it may be worth checking this
+    idx = flight_info[flight_info['Flight Number'] == 333].index[0]
+
+    # Get information about the flight to fill out the filename
+    datestr = flight_info['Date'][idx].replace('-', '')
+    revision = flight_info['Revision'][idx]
+
+    filename = 'obs/core_masin_{}_r{:03}_flight{}_{}hz.nc'.format(
+        datestr, revision, flight_number, frequency)
+    return filename
