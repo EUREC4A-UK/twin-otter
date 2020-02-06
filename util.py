c6bde9e445f66abd1cf83ff3f70506b829732c54
Leif Denby <leif@denby.eu>
2020-02-06 16:17:18 -0400

    parse times when loading

--- util.py
+++ util.py
@@ -9,9 +9,7 @@ def load_flight(flight_number, frequency=1):
     datestr = flight_info['Date'][flight_info['Flight Number'] == flight_number].values[0].replace('-', '')
     filename = 'obs/core_masin_{}_r002_flight{}_{}hz.nc'.format(datestr, flight_number, frequency)
 
-    # because of how the flags are stored (units is '1b') decoding cf times
-    # fails
-    ds = xr.open_dataset(filename, decode_times=False)
+    ds = xr.open_dataset(filename, decode_cf=False)
 
     # plot as function of time
     ds = ds.swap_dims(dict(data_point='Time'))
