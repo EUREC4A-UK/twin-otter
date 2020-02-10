c33b763d5f93824fbc22a5c6c8952e2db9ba3e98
lsaffin <l.saffin@leeds.ac.uk>
2020-02-10 07:16:38 -0400

    Allow a file or directory to be specified for loading a dataset

--- twinotter/__init__.py
+++ twinotter/__init__.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import re
 
 import pandas as pd
 import xarray as xr
@@ -9,32 +10,69 @@ MASIN_CORE_FORMAT = "core_masin_{date}_r{revision}_flight{flight_number}_{freque
 MASIN_CORE_RE = "core_masin_(?P<date>\d{8})_r(?P<revision>\d{3})_flight(?P<flight_num>\d{3})_(?P<freq>\d+)hz\.nc"
 
 
-def load_flight(flight_number, frequency=1, revision="most_recent", debug=False):
-    filename = generate_file_path(flight_number, frequency)
+def load_flight(flight_data_path, frequency=1, revision="most_recent", debug=False):
+    # If a path to a netCDF file is specified just load it
+    if Path(flight_data_path).is_file():
+        meta = re.match(MASIN_CORE_RE, Path(flight_data_path).name).groupdict()
+        return open_masin_dataset(flight_data_path, meta, debug=debug)
 
+    # Otherwise a directory is supplied so look for files that match within
+    # the given directory
+
+    # If you want to use the most recent revision, get the filenames of all the
+    # revisions and choose the newest one
     if revision == "most_recent":
         revision = "*"
     else:
         revision = "{:03d}".format(revision)
 
+    fn_pattern = MASIN_CORE_FORMAT.format(
+        date="*", revision=revision, flight_num="*", freq=frequency
+    )
+    files = list((Path(flight_data_path)/"MASIN").glob(fn_pattern))
+
+    meta = {}
+    for file in files:
+        meta[file] = re.match(MASIN_CORE_RE, file.name).groupdict()
+
+    if len(files) == 0:
+        raise Exception("Couldn't find MASIN data in `{}/MASIN`, please place"
+                        " data there.".format(flight_data_path))
+
+    if len(files) > 1:
+        if revision == "*":
+            filename = sorted(files, key=lambda v: meta[v]['revision'], reverse=True)[0]
+        else:
+            raise Exception("More than one MASIN file was found: `{}`".format(
+                ", ".join(files)
+            ))
+    else:
+        filename = files[0]
+
+    ds = open_masin_dataset(filename, meta, debug=debug)
+
+    return ds
+
+
+def open_masin_dataset(filename, meta, debug=False):
     ds = xr.open_dataset(filename, decode_cf=False)
     if debug:
         print("Loaded {}".format(filename))
 
     # drop points where lat/lon aren't given (which means the flag is 0
     # "quality_good"
-    ds = ds.where(ds.LON_OXTS_FLAG==0, drop=True)
+    ds = ds.where(ds.LON_OXTS_FLAG == 0, drop=True)
     # drop nans too...
     ds = ds.where(~ds.LON_OXTS.isnull(), drop=True)
 
     # XXX: quick fix until we get loading with CF-convention parsing working
-    ds = ds.where(ds.LON_OXTS!=ds.LON_OXTS._FillValue, drop=True)
+    ds = ds.where(ds.LON_OXTS != ds.LON_OXTS._FillValue, drop=True)
 
     # plot as function of time
     ds = ds.swap_dims(dict(data_point='Time'))
 
     ds.attrs['source_file'] = filename
-    ds.attrs['flight_number'] = meta[filename]['flight_num']
+    ds.attrs['flight_number'] = meta['flight_num']
 
     return ds
 
