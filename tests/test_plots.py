23c95afa2ebd84d7f26ce3fc5322a7f81f9e48e8
Leif Denby <leif@denby.eu>
2020-04-02 17:08:01 +0100

    add test for profile plots

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -1,13 +1,18 @@
 from unittest.mock import patch
 
 import twinotter.plots.basic_flight_track
-import twinotter.plots.interactive_flight_track
+import twinotter.plots.vertical_profile
 
 
 @patch('matplotlib.pyplot.savefig')
 def test_basic_flight_path(mock_savefig):
     twinotter.plots.basic_flight_track.generate(flight_data_path="obs/flight330")
     mock_savefig.assert_called_once()
+
+@patch('matplotlib.pyplot.show')
+def test_vertical_profile_plot(mock_showfig):
+    twinotter.plots.vertical_profile.main(flight_data_path="obs/flight330")
+    mock_showfig.assert_called_once()
 
 # this doesn't run properly because of the gui needing to be opened
 # @patch('matplotlib.pyplot.show')
