f30534a37d1992bd0a5de657b6fc646350b8b680
Leif Denby <leif@denby.eu>
2020-04-04 16:25:37 +0100

    add test for quicklook profile and leg plots

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -1,11 +1,73 @@
 from unittest.mock import patch
+import pytest
 import tempfile
 import os
+from pathlib import Path
+import requests
 
 import twinotter.plots.basic_flight_track
 import twinotter.plots.vertical_profile
 import twinotter.plots.heights_and_legs
 import twinotter.quicklook
+
+TESTDATA_URL = "http://homepages.see.leeds.ac.uk/~earlcd/eurec4a/testdata/twinotter.testdata.tar.gz"
+
+
+def untar_data(url, fname, dest):
+    "Download `url` to `fname` if `dest` doesn't exist, and un-tgz to folder `dest`."
+    dest = url2path(url, data) if dest is None else Path(dest)/url2name(url)
+    fname = Path(ifnone(fname, _url2tgz(url, data)))
+    if force_download or (fname.exists() and url in _checks and _check_file(fname) != _checks[url]):
+        print(f"A new version of the {'dataset' if data else 'model'} is available.")
+        if fname.exists(): os.remove(fname)
+        if dest.exists(): shutil.rmtree(dest)
+    if not dest.exists():
+        fname = download_data(url, fname=fname, data=data)
+        if url in _checks:
+            assert _check_file(fname) == _checks[url], f"Downloaded file {fname} does not match checksum expected! Remove that file from {Config().data_archive_path()} and try your code again."
+    return dest
+
+
+class TestData():
+    def __init__(self, url):
+        self.url = url
+
+    def __enter__(self):
+        self.fhtar = tempfile.NamedTemporaryFile(
+            delete=False, mode="w", suffix='.tar.gz'
+        )
+        r = requests.get(TESTDATA_URL)
+        self.fhtar.write(r.content)
+        self.fhtar.close()
+
+        tarfile.open(fname, 'r:gz').extractall(dest.parent)
+
+    def __exit__(self):
+        os.unlink(fhtar.name)
+
+    @property
+    def path(self):
+        return self.fh.name
+
+@pytest.fixture
+def testdata_single_flight(scope="module"):
+    # "leg"-files are created interactively, so we just add the content for one
+    # here and write that to a temporary file
+    fh = tempfile.NamedTemporaryFile(delete=False, mode="w", suffix='.tar.gz')
+    try:
+        r = requests.get(TESTDATA_URL)
+        fh.write(r.content)
+        fh.close()
+
+        twinotter.plots.heights_and_legs.generate(
+            flight_data_path="obs/flight330",
+            legs_file=fh.name
+        )
+        mock_savefig.assert_called_once()
+    finally:
+        os.unlink(fh.name)
+
+
 
 
 @patch('matplotlib.pyplot.savefig')
@@ -65,7 +127,15 @@ def test_quicklook_plot(mock_savefig):
             flight_data_path="obs/flight330",
             legs_file=fh.name
         )
-        mock_savefig.assert_called_with()
+        n_legs = len(LEGS_FILE_FLIGHT330.split('Leg'))-1
+        for n in range(n_legs):
+            fn_fig = "flight{}/Leg{}_quicklook.png".format(330, n)
+            mock_savefig.assert_any_call(fn_fig)
+
+        n_profiles = len(LEGS_FILE_FLIGHT330.split('Profile'))-1
+        for n in range(n_profiles):
+            fn_fig = "flight{}/Profile{}_skewt.png".format(330, n)
+            mock_savefig.assert_any_call(fn_fig)
     finally:
         os.unlink(fh.name)
 
