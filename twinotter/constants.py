dcb1288786f6c6053f32e46dcbb55f448fb35135
Leo Saffin <l.saffin@leeds.ac.uk>
2020-04-22 16:34:27 +0100

    Use constants already available from metpy

--- twinotter/constants.py
+++
@@ -1,5 +0,0 @@
-
-# Mass per mole of dry air and water vapour (kg mol-1)
-# Taken from https://www.licor.com/env/support/EddyPro/topics/calculate-micromet-variables.html
-m_d = 0.02897
-m_h20 = 0.01802
--- twinotter/derive.py
+++ twinotter/derive.py
@@ -1,9 +1,12 @@
-from twinotter import constants
+from metpy import constants
 
 
 def specific_humidity(dataset):
 
     x_h20 = dataset.H2O_LICOR
-    q = constants.m_h20 * x_h20 / (constants.m_h20 * x_h20 + constants.m_d*(1-x_h20))
+    q = constants.water_molecular_weight * x_h20 / (
+            constants.water_molecular_weight * x_h20 +
+            constants.dry_air_molecular_weight*(1-x_h20)
+    )
 
     return q
