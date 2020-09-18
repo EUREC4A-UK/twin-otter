862e5b2d770f5b3a9ba68051d18d2cbf74b445e0
Leo Saffin <LSaffin@users.noreply.github.com>
2020-09-18 10:44:59 +0100

    Derive variables (#14)
    * Added humidity derived from LICOR data to quicklooks
    
    * Use constants already available from metpy
    
    * Added setup for calculating variables by name with example for potential temperature
    
    * Added translation table between twinotter var names and CF standard names
    
    * Added function for combining temperatures (nondeiced and deiced) and fixed to automatically get from the calculate function
    
    * Updated quicklook to be able to produce multiple figures per category and avoid LICOR if it is not available
    
    * Added docstring and TODOs
    
    * Added testing for derive
    
    * Added theta_e and theta_v. Currently ignoring HUM_ROSE which was broken for EUREC4A
    
    * Add function for along-track wind

--- twinotter/quicklook.py
+++ twinotter/quicklook.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-from . import load_flight, load_legs, leg_times_as_datetime
+from . import load_flight, load_legs, leg_times_as_datetime, derive
 from .plots import vertical_profile
 
 
@@ -38,6 +38,7 @@ def generate(flight_data_path, legs_file):
     leg_times_as_datetime(legs, ds.Time[0].dt.floor('D').data)
 
     path_figures = Path(flight_data_path)/"figures"
+    path_figures.mkdir(exist_ok=True)
 
     counters = dict(Leg=0, Profile=0)
 
@@ -46,69 +47,82 @@ def generate(flight_data_path, legs_file):
 
         n = counters[label]
         if label == 'Leg':
-            fn = '{}{}_{}.png'.format(label, n, 'quicklook')
             plot_func = plot_leg
         elif label == 'Profile':
-            fn = '{}{}_{}.png'.format(label, n, 'skewt')
             plot_func = plot_profile
         else:
             raise NotImplementedError(label)
         counters[label] += 1
 
         ds_section = ds.sel(Time=slice(leg.Start, leg.End))
-        fig = plot_func(ds_section)
-        path_figures.mkdir(exist_ok=True)
-        plt.savefig(str(path_figures/fn))
-        plt.close()
+        figures = plot_func(ds_section)
+
+        for fig, figname in figures:
+            fn = '{}{}_{}.png'.format(label, n, figname)
+            print(str(path_figures/fn))
+            fig.savefig(str(path_figures/fn))
+            plt.close(fig)
 
 
 def plot_leg(ds):
-    fig, axes = plt.subplots(nrows=5, ncols=1, sharex=True, figsize=[16, 15])
+    fig1, axes1 = plt.subplots(nrows=5, ncols=1, sharex='all', figsize=[16, 15])
 
     # Temperature and Dewpoint
-    ds.TAT_ND_R.plot(ax=axes[0], label=r'True')
-    ds.TDEW_BUCK.plot(ax=axes[0], label=r'Dewpoint')
-    add_labels(axes[0], 'Temperature (K)')
-    axes[0].legend()
+    ds.TAT_ND_R.plot(ax=axes1[0], label=r'True')
+    ds.TDEW_BUCK.plot(ax=axes1[0], label=r'Dewpoint')
+    add_labels(axes1[0], 'Temperature (K)')
+    axes1[0].legend()
 
     # Velocities
-    ds.U_OXTS.plot(ax=axes[1], label=r'Zonal')
-    ds.V_OXTS.plot(ax=axes[1], label=r'Meridional')
-    add_labels(axes[1], 'Velocity (m s$^{-1}$)')
-    axes[1].legend()
+    ds.U_OXTS.plot(ax=axes1[1], label=r'Zonal')
+    ds.V_OXTS.plot(ax=axes1[1], label=r'Meridional')
+    add_labels(axes1[1], 'Velocity (m s$^{-1}$)')
+    axes1[1].legend()
 
-    ds.W_OXTS.plot(ax=axes[2])
-    add_labels(axes[2], 'Vertical Velocity (m s$^{-1}$)')
+    ds.W_OXTS.plot(ax=axes1[2])
+    add_labels(axes1[2], 'Vertical Velocity (m s$^{-1}$)')
 
     # Shortwave radiation
-    ds.SW_DN_C.plot(ax=axes[3], label=r'SW Downwelling')
-    ds.SW_UP_C.plot(ax=axes[3], label=r'SW Upwelling')
-    axes[3].legend()
+    ds.SW_DN_C.plot(ax=axes1[3], label=r'SW Downwelling')
+    ds.SW_UP_C.plot(ax=axes1[3], label=r'SW Upwelling')
+    axes1[3].legend()
 
     # Longwave radiation
-    ds.LW_DN_C.plot(ax=axes[3], label=r'LW Downwelling')
-    ds.LW_UP_C.plot(ax=axes[3], label=r'LW Upwelling')
-    add_labels(axes[3], 'Irradiance (W m$^{-2}$)')
-    axes[3].legend()
+    ds.LW_DN_C.plot(ax=axes1[3], label=r'LW Downwelling')
+    ds.LW_UP_C.plot(ax=axes1[3], label=r'LW Upwelling')
+    add_labels(axes1[3], 'Irradiance (W m$^{-2}$)')
+    axes1[3].legend()
 
     # CPC Concentration
-    ds.CPC_CONC.plot(ax=axes[4])
-    add_labels(axes[4], 'CPC Concentration (m$^{-3}$)')
+    ds.CPC_CONC.plot(ax=axes1[4])
+    add_labels(axes1[4], 'CPC Concentration (m$^{-3}$)')
 
-    return fig
+    # Not all flights have LICOR data
+    try:
+        fig2, axes2 = plt.subplots(nrows=2, ncols=1, sharex='all', figsize=[16, 15])
+        # LICOR data
+        ds.CO2_LICOR.plot(ax=axes2[0], label=r"CO$_2$ LICOR")
+        ds.H2O_LICOR.plot(ax=axes2[0], label=r"H$_2$O LICOR")
+        add_labels(axes2[0], "Mole Fraction")
+        axes2[0].legend()
+
+        axes2[1].plot(ds.Time, derive.specific_humidity(ds))
+        add_labels(axes2[1], "Specific Humidity")
+
+        return [(fig1, "quicklook"), (fig2, "quicklook_LICOR")]
+    except AttributeError:
+        return [(fig1, "quicklook")]
 
 
 def plot_profile(dataset):
-    fig, ax = plt.subplots()
-
     p = dataset.PS_AIR
     T = dataset.TAT_ND_R - 273.15
     Td = dataset.TDEW_BUCK - 273.15
     u = dataset.U_OXTS
     v = dataset.V_OXTS
-    vertical_profile.skewt(p, T, Td, u, v)
+    fig = vertical_profile.skewt(p, T, Td, u, v)
 
-    return fig
+    return [(fig, "skewt")]
 
 
 def add_labels(ax, ylabel):
