b1c63d4bccec362f516abb18bbbb484e0df6287d
Leo Saffin <l.saffin@leeds.ac.uk>
2021-03-15 12:06:05 +0000

    Allow for extra segment categories with undefined colors in heights_and_legs.py

--- twinotter/plots/heights_and_legs.py
+++ twinotter/plots/heights_and_legs.py
@@ -19,32 +19,47 @@ def main():
     argparser.add_argument("flight_data_path")
     argparser.add_argument("flight_segments_file")
     argparser.add_argument("--show-gui", default=False, action="store_true")
+    argparser.add_argument("--output_path", default=None)
     args = argparser.parse_args()
 
-    generate(args.flight_data_path, args.flight_segments_file, show_gui=args.show_gui)
+    generate(args.flight_data_path, args.flight_segments_file,
+             show_gui=args.show_gui, output_path=args.output_path)
 
 
-def generate(flight_data_path, flight_segments_file, show_gui=False):
+def generate(flight_data_path, flight_segments_file, show_gui=False, output_path=None):
     ds = load_flight(flight_data_path)
     segments = load_segments(flight_segments_file)
 
     # Produce the basic time-height plot
     fig, ax1 = plt.subplots()
-    ax1.plot(ds.Time, ds.ROLL_OXTS, color="k", linestyle="--", alpha=0.5)
+    ax1.plot(ds.Time, ds.ROLL_OXTS, color="k", linestyle="--", alpha=0.1)
     ax1.set_ylabel("Roll Angle")
     ax2 = ax1.twinx()
-    ax2.plot(ds.Time, ds.ALT_OXTS / 1000, color="k")
+    ax2.plot(ds.Time, ds.ALT_OXTS / 1000, color="k", alpha=0.5)
     ax2.set_ylabel("Altitude (km)")
 
-    # For each leg overlay a coloured line onto the time-height plot
-    for leg in tqdm(segments["segments"]):
-        ds_section = ds.sel(Time=slice(leg["start"], leg["end"]))
+    # For each segment overlay a coloured line onto the time-height plot
+    for segment in tqdm(segments["segments"]):
+        ds_section = ds.sel(Time=slice(segment["start"], segment["end"]))
+        label = segment["kinds"][0]
 
-        label = leg["kinds"][0]
+        linestyle = "-"
+        try:
+            color = colors[label]
+        except KeyError:
+            # If the primary segment type doesn't have an assigned colour but one of the
+            # other "kinds" matches then use that colour with a dashed line
+            color = "yellow"
+            linestyle = "--"
+            for segment_type in colors:
+                if segment_type in segment["kinds"]:
+                    color = colors[segment_type]
+
         ax2.plot(
             ds_section.Time,
             ds_section.ALT_OXTS / 1000,
-            color=colors[label],
+            linestyle=linestyle,
+            color=color,
             linewidth=2,
             alpha=0.75,
         )
@@ -63,9 +78,13 @@ def generate(flight_data_path, flight_segments_file, show_gui=False):
     if show_gui:
         plt.show()
     else:
-        p = Path(flight_data_path) / "figures" / "height-time-with-legs.png"
-        p.parent.mkdir(exist_ok=True)
-        plt.savefig(str(p), bbox_inches="tight")
+        if output_path is None:
+            output_path = Path(flight_data_path) / "figures" / "height-time-with-legs.png"
+        else:
+            output_path = Path(output_path)
+
+        output_path.parent.mkdir(exist_ok=True)
+        plt.savefig(str(output_path), bbox_inches="tight")
 
 
 if __name__ == "__main__":
